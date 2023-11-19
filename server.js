87b090549209dbfff75d9a6810a6442a1fabf121
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-11-20 01:25:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get(`${API_VERSION}/users`, async (req, res) => {
   try {
     const response = await axios.get(`${JSON_SERVER_URL}/users`);
     const users = response.data;
+
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
@@ -24,14 +25,19 @@ app.get(`${API_VERSION}/users`, async (req, res) => {
 // Get a specific user by ID
 app.get(`${API_VERSION}/users/:id`, async (req, res) => {
   const userId = req.params.id;
+
   try {
     const response = await axios.get(`${JSON_SERVER_URL}/users/${userId}`);
     const user = response.data;
-    if (!user || Object.keys(user).length === 0) {
-      res.status(404).json({ message: "No user found" });
-    } else {
-      res.status(200).json(user);
-    }
+
+    // if (response) {
+    //   const user = response.data;
+    //   res.status(200).json(user);
+    // } else {
+    //   res.status(404).json({ message: "No user found" });
+    // }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
@@ -40,10 +46,51 @@ app.get(`${API_VERSION}/users/:id`, async (req, res) => {
 // Add a new user
 app.post(`${API_VERSION}/users`, async (req, res) => {
   const newUser = req.body;
+
   try {
     const response = await axios.post(`${JSON_SERVER_URL}/users`, newUser);
-    const addedUser = response.data;
-    res.status(201).json(addedUser);
+    // const addedUser = response.data;
+    // res.status(201).json(addedUser);
+
+    res.status(201).json({ message: "User added successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Update a specific user by ID
+app.put(`${API_VERSION}/users/:id`, async (req, res) => {
+  const userId = req.params.id;
+  const updatedUserData = req.body;
+
+  try {
+    const response = await axios.put(`${JSON_SERVER_URL}/users/${userId}`, updatedUserData);
+    // const updatedUser = response.data;
+
+    // if (response) {
+    //   res.status(200).json({ message: "User data updated successfully" });
+    // } else {
+    //   res.status(404).json({ message: "No user found" });
+    // }
+
+    res.status(200).json({ message: "User data updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Delete a specific user by ID
+app.delete(`${API_VERSION}/users/:id`, async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const response = await axios.delete(`${JSON_SERVER_URL}/users/${userId}`);
+    
+    if (response.status === 200) {
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "No user found" });
+    }
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
